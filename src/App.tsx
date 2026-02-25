e9eb08a88eea49f8e351a158a298922554ef1bf8
Pruthviraj Deshmukh <raj@Pruthvirajs-MacBook-Pro.local>
2026-02-25 15:13:51 +0530

    feat: set up React Router with Home route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
-      <h1>React Signup and Login Forms</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route index path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
